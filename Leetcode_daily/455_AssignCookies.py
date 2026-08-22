7e7882cdc91fad25d1f133de3e23bfeca0dfcb2d
vlakshitha6127 <vlakshitha6127@gmail.com>
2026-08-22 23:01:56 +0530

    Leetcode 455 solution

--- Leetcode_daily/455_AssignCookies.py
+++ Leetcode_daily/455_AssignCookies.py
@@ -5,14 +5,14 @@ class Solution:
 
         i = 0
         j = 0
-        count = 0
+        co = 0
 
         while i < len(g) and j < len(s):
             if s[j] >= g[i]:
-                count += 1
+                co += 1
                 i += 1
                 j += 1
             else:
                 j += 1
 
-        return count
+        return co
